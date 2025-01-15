d44eb7547effa59158ac563077f356544e0d6620
qc_f142df755deb4342b776331ccc96f8b1 <qc_f142df755deb4342b776331ccc96f8b1@git.code.tencent.com>
2025-01-15 23:57:55 +0800

    实现了一个基础预览

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,16 +1,83 @@
 <script setup>
+import {computed} from "vue";
 
+const props = defineProps({
+  facePid: {
+    type: String,
+    required: true
+  },
+  quickLookPid: {
+    type: String,
+    required: true
+  },
+  videoName: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+})
+
+const imageUrl = computed(() => {
+  return `/api/face-image/${props.facePid}`
+})
+
+const videoUrl = computed(() => {
+  return `/api/quick-look/${props.quickLookPid}`
+})
+
+const age = computed(() => {
+  return Math.round(props.age)
+})
+
+
+const duration = computed(() => {
+  const totalSeconds = Math.floor(props.duration);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+  return `${hours.toString()}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+})
 </script>
 
 <template>
-  <v-responsive
-    :aspect-ratio="1"
-    class="border"
-  >
-    <div>hello world</div>
-  </v-responsive>
+  <v-hover>
+    <template #default="{ isHovering ,props}">
+      <v-responsive
+        :aspect-ratio="1"
+        v-bind="props"
+      >
+        <v-sheet
+          v-if="isHovering"
+          class="ma-0 pa-0"
+        >
+          <video
+            :src="videoUrl"
+            muted
+            loop
+            autoplay
+          />
+          <div>{{ props.videoName }}</div>
+        </v-sheet>
+        <v-img
+          v-else
+          :src="imageUrl"
+          :aspect-ratio="1"
+          class="ma-0 pa-0"
+        />
+      </v-responsive>
+    </template>
+  </v-hover>
 </template>
 
 <style scoped>
-
+video {
+  width: 100%;
+}
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,47 @@
 <template>
   <v-row no-gutters>
     <v-col
-      v-for="item in Array(256)"
+      v-for="previewItem in previewArray"
+      :key="previewItem.video_pid"
       cols="1"
       md="4"
       class="pa-1"
     >
-      <preview />
+      <preview
+        :face-pid="previewItem.face_pid"
+        :quick-look-pid="previewItem.quick_look_pid"
+        :video-name="previewItem.name"
+        :age="previewItem.age"
+        :duration="previewItem.duration"
+      />
     </v-col>
   </v-row>
 </template>
 
 <script setup>
 import Preview from "@/components/Preview.vue";
+import {ref,onMounted} from 'vue'
+import axios from "axios";
+
+const previewArray  = ref([])
+const loadBatchSize = ref(20)
+const pageLoading = ref(false)
+const exhaustedMark = ref(false)
+const fetcher = async () =>{
+  const current = previewArray.value.length
+  pageLoading.value = true
+  try {
+    const body = {offset: current, limit: loadBatchSize.value}
+    const response = await axios.post('/api/list-preview', body)
+    if (response.data.length ===0){
+      exhaustedMark.value = true
+    } else {
+          previewArray.value.push(...response.data)
+    }
+  } finally {
+    pageLoading.value =false
+  }
+}
+
+onMounted(fetcher)
 </script>
