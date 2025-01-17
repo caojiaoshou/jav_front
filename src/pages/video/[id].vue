62aae3433e6f961f32ab6b7121f8feb5e3eed348
qc_f142df755deb4342b776331ccc96f8b1 <qc_f142df755deb4342b776331ccc96f8b1@git.code.tencent.com>
2025-01-17 22:39:39 +0800

    完善播放页

--- src/pages/video/[id].vue
+++ src/pages/video/[id].vue
@@ -2,11 +2,25 @@
 import {useRoute} from "vue-router";
 import {computed, onMounted, ref} from 'vue';
 import axios from 'axios';
+import {useDisplay} from "vuetify";
 
 const route = useRoute();
 const pageID = route.params.id;
 
 const sceneArray = ref([]);
+
+const posterImage = computed(() => {
+  if (sceneArray.value.length === 0) {
+    return ''
+  } else {
+    for (let sceneItem in sceneArray) {
+      if (sceneItem.reason === 'face') {
+        return sceneItem.preview;
+      }
+    }
+    return sceneArray.value[0].preview
+  }
+})
 
 const fetchMeta = async () => {
   const response = await axios.post('/api/video-ts', {video_pid: pageID});
@@ -26,32 +40,40 @@ const seekToTime = (time) => {
   if (videoPlayer.value) {
     videoPlayer.value.currentTime = time;
   }
-};
+}
+
+const display = useDisplay();
+const boxWidth = computed(() => {
+  return display.lgAndUp.value ? '70vw' : '100vw'
+});
 </script>
 
 <template>
-  <v-responsive width="80%">
-    <video
-      ref="videoPlayer"
-      :src="videoUrl"
-      controls
-      width="100%"
-    />
-
-
-    <v-row>
-      <v-col
-        v-for="scene in sceneArray"
-        :key="scene.ts"
-        cols="2"
-      >
-        <v-img
-          :src="scene.preview"
-          @click="seekToTime(scene.ts)"
-        />
-      </v-col>
-    </v-row>
-  </v-responsive>
+  <v-container class="d-flex align-center">
+    <v-responsive :width="boxWidth">
+      <video
+        ref="videoPlayer"
+        :src="videoUrl"
+        controls
+        preload="metadata"
+        width="100%"
+        :poster="posterImage"
+        controlslist="nodownload"
+      />
+      <v-row>
+        <v-col
+          v-for="scene in sceneArray"
+          :key="scene.ts"
+          cols="2"
+        >
+          <v-img
+            :src="scene.preview"
+            @click="seekToTime(scene.ts)"
+          />
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
 </template>
 
 <style scoped>
