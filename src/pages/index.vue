8d8ceb5250fb2fbe578f4da964a63b96c1ca0fec
qc_f142df755deb4342b776331ccc96f8b1 <qc_f142df755deb4342b776331ccc96f8b1@git.code.tencent.com>
2025-01-26 20:12:36 +0800

    增加了一个混合筛选功能

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row no-gutters>
     <v-col
-      v-for="(previewItem,previewIndex) in previewArray"
+      v-for="(previewItem,previewIndex) in filteredPreviews"
       :key="previewItem.video_pid"
       cols="6"
       md="4"
@@ -24,24 +24,45 @@
       />
     </v-col>
   </v-row>
+  <div v-intersect="onIntersect" />
 </template>
 
 <script setup>
 import Preview from "@/components/Preview.vue";
-import {onMounted, ref} from 'vue'
+import {onMounted, ref, watch} from 'vue'
 import axios from "axios";
 import {useRouter} from 'vue-router'
+import {useGlobalFilter} from "@/stores/app.js";
+
+const globalFilter = useGlobalFilter()
 
 const previewArray = ref([])
 const loadBatchSize = ref(20)
 const pageLoading = ref(false)
 const exhaustedMark = ref(false)
+watch(
+  () => globalFilter.textValue,
+  () => {
+    console.log('watched')
+    exhaustedMark.value = false
+  }
+)
+
+const filteredPreviews = computed(() => {
+  if (globalFilter.textValue) {
+    return previewArray.value.filter(item => {
+      return item.name.includes(globalFilter.textValue)
+    })
+  } else {
+    return previewArray.value
+  }
+})
 
 const fetcher = async () => {
-  const current = previewArray.value.length
+  const current = filteredPreviews.value.length
   pageLoading.value = true
   try {
-    const body = {offset: current, limit: loadBatchSize.value}
+    const body = {offset: current, limit: loadBatchSize.value, filter: globalFilter.textValue}
     const response = await axios.post('/api/list-preview', body)
     if (response.data.length === 0) {
       exhaustedMark.value = true
@@ -56,9 +77,8 @@ const fetcher = async () => {
 onMounted(fetcher)
 
 
-const onIntersect = (entry) => {
-  console.log(entry)
-  if (entry && !pageLoading.value && !exhaustedMark.value) {
+const onIntersect = (bEntryState) => {
+  if (bEntryState && !pageLoading.value && !exhaustedMark.value) {
     fetcher()
   }
 }
